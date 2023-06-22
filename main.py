9baa446e1a9243179ebe5716bf0f71de7c98b7e2
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-06-23 01:33:58 +0530

    code to read no. of words in a books

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+
+def num_of_words(book):
+    words = book.split()
+    return len(words)
+
+print(num_of_words(file_contents))
+
