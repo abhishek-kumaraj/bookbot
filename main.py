6d56259a0c776844e38998c7a0ba63fcd048e909
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-06-23 02:26:23 +0530

    Added main method to print the final report of the book

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def main():
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_of_words(file_contents)} words found in the document")
+
+    my_dic = num_of_each_char(file_contents)
+    my_list = sorted(my_dic.items(), key=lambda x: x[1], reverse=True)
+    orted_keys = [item[0] for item in my_list]
+    for item in orted_keys:
+        print(f"The '{item}' character was found {my_dic[item]} times")
+    print("--- End report ---")
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
@@ -10,13 +21,14 @@ def num_of_each_char(book):
     book = book.lower()
     mySet = set(book)
     for element in mySet:
-        countOfChar = 0
-        for character in book:
-            if character == element:
-                countOfChar += 1
-        my_dic[element] = countOfChar
+        if element.isalpha():
+            countOfChar = 0
+            for character in book:
+                if character == element:
+                   countOfChar += 1
+            my_dic[element] = countOfChar
+        
     return my_dic   
-      
+    
 
-print(num_of_each_char(file_contents))
-
+main()
